04195095331f1f86e813b0f9ced36cab3314b64c
Ugur <ugurugurov11@gmail.com>
2025-10-26 21:57:38 +0300

    enhance code

--- training/challenges.js
+++ training/challenges.js
@@ -1,8 +1,7 @@
-console.log(Boolean(""))
-console.log(Boolean("0"))
-console.log(Boolean(0))
-console.log(Boolean(100))
-console.log(Boolean(null))
-console.log(Boolean([0]))
-console.log(Boolean(-0))
+const myArr = [1324, 345234, 3245]
 
+function getFirst(arr) {
+    return arr[0]
+}
+
+getFirst(myArr)
