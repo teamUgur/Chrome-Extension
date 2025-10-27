16b375948da07971bc374b649fe499b29c003f21
Ugur <ugurugurov11@gmail.com>
2025-10-27 11:15:17 +0300

    save tab button works

--- main-project/scripts.js
+++ main-project/scripts.js
@@ -4,6 +4,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById('tab-btn')
 
 // save history even after reloading page
 const leadsFromLocalStorage  = JSON.parse(localStorage.getItem("myLeads"))
@@ -11,6 +12,18 @@ if (leadsFromLocalStorage) { // if leadsFromLocalStorage not empty
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+tabBtn.addEventListener("click", function() {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+    // active: true - tabs where we are now
+    // currentWindow: true - if you have 2 windows of chrome - active one - will be used
+    // function in chrome.tabs.query will be triggered after active: true, currentWindow: true
+})
 
 // add leads to function, myLeads equals to leads, we put it above
 function render(leads) {
